076a9611d56b1d25df1196fda85abe62c82ea9f7
Anna Veselova <anna.veselova.97@mail.ru>
2023-10-16 17:12:53 +0300

    delete comments

--- places/admin.py
+++ places/admin.py
@@ -4,18 +4,6 @@ from adminsortable2.admin import SortableInlineAdminMixin, SortableAdminMixin
 from places.models import Place, Image
 
 
-# class ImageInline(SortableInlineAdminMixin, admin.TabularInline):
-#     model = Image
-#     extra = 1
-#     sortable = 'image_num'
-#     readonly_fields = ['image_preview',]
-#     fields = ['image', 'image_preview', 'image_num',]
-
-#     def image_preview(self, instance):
-#         return format_html('<img src="{}" style="max-height: 200px;" />', instance.image.url)
-
-#     image_preview.short_description = 'Превью'
-
 class ImageInlineAdmin(SortableInlineAdminMixin, admin.TabularInline):
     model = Image
     extra = 1
@@ -38,5 +26,3 @@ class PlaceAdmin(SortableAdminMixin, admin.ModelAdmin):
     search_fields = ['title',]
     list_display = ['title', 'id',]
     inlines = [ImageInlineAdmin,]
-
-# admin.site.register(Image)
